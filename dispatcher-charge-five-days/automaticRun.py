2f7baec5c93a3b0dba9f5c3bc8f48f3c439464e4
Clswe <cristian.luiz@outlook.com.br>
2025-04-24 13:29:01 -0300

    correção automaticRun

--- dispatcher-charge-five-days/automaticRun.py
+++ dispatcher-charge-five-days/automaticRun.py
@@ -1,14 +1,23 @@
 import subprocess
+import os
 
 def run_script(script_name):
+    script_path = os.path.join(base_dir, script_name)
     try:
-        result = subprocess.run(['python3', script_name], check=True)
+        result = subprocess.run(['python3', script_path], check=True)
         print(f"{script_name} executed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error occurred while executing {script_name}: {e}")
 
+# Caminho base (diretório onde este script está localizado)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
 # Lista de scripts a serem executados sequencialmente
-scripts = ['oral-platinum-cobran-a-dispatcher/dispatcher-charge-five-days/find_charge.py', 'oral-platinum-cobran-a-dispatcher/dispatcher-charge-five-days/contact_manager.py', 'oral-platinum-cobran-a-dispatcher/dispatcher-charge-five-days/send_mensage.py']
+scripts = [
+    'oral-platinum-cobran-a-dispatcher/dispatcher-charge-five-days/find_charge.py',
+    'oral-platinum-cobran-a-dispatcher/dispatcher-charge-five-days/contact_manager.py',
+    'oral-platinum-cobran-a-dispatcher/dispatcher-charge-five-days/send_mensage.py'
+]
 
 for script in scripts:
     run_script(script)
